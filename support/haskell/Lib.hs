5548e2b5e6a10cf816aceaebf800d8db6506bae6
Alexander Thiemann <mail@athiemann.net>
2015-10-30 17:50:09 +0100

    bugfixes to haskell library

--- support/haskell/Lib.hs
+++ support/haskell/Lib.hs
@@ -9,9 +9,10 @@ where
 
 import Data.Aeson
 import Data.Aeson.Types
-import data.Time
+import Data.Time
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base64 as B64
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 -- | Usual bytestrings, but with base64 aeson repr
@@ -36,17 +37,15 @@ instance FromJSON AsBase64 where
 eatBool :: Bool -> Parser ()
 eatBool _ = return ()
 
--- Extremely hacky until next aeson version includes TimeOfDay
+-- Hacky until next aeson version includes TimeOfDay
 instance ToJSON TimeOfDay where
     toJSON tod =
-        case toJSON (LocalTime (ModifiedJulianDay 0) tod) of
-          Text x ->
-              Text (T.drop 11 x)
-          _ -> error "Library error!"
+        String $ T.pack $ formatTime defaultTimeLocale "%H:%M:%S" tod
 
--- Extremely hacky until next aeson version includes TimeOfDay
+-- Hacky until next aeson version includes TimeOfDay
 instance FromJSON TimeOfDay where
     parseJSON =
         withText "TimeOfDay" $ \t ->
-        do localTime <- parseJSON (Text $ "1994-02-04 " <> t)
-           return $ localTimeOfDay localTime
+        case parseTimeM True defaultTimeLocale "%H:%M:%S" (T.unpack t) of
+          Nothing -> fail "Invalid time of day"
+          Just tod -> return tod
